b1cda9a7e7ba6495c9f2d961f346918ecd2b71d2
Khanh Duong <khanhduong4036@gmail.com>
2023-11-25 14:43:33 +0700

    Day-16, Component Class

--- src/main/java/models/pages/BasePage.java
+++ src/main/java/models/pages/BasePage.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.WebDriver;
 
 public class BasePage {
 
-    private final WebDriver driver;
+    protected final WebDriver driver;
 
     public BasePage(WebDriver driver){
         this.driver = driver;
