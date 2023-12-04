dcc150eb6be2c3b7de0c2dd36526612b8c1afdd6
Khanh Duong <khanhduong4036@gmail.com>
2023-12-04 23:49:43 +0700

    Day-17

---
+++ src/main/java/tests/verifier/VerifierTest.java
@@ -0,0 +1,15 @@
+package tests.global.footer;
+
+import org.testng.annotations.Test;
+import test_flows.global.FooterTestFlow;
+import tests.BaseTest;
+
+public class FooterTest extends BaseTest {
+
+    @Test
+    public void testHomePageFooter() {
+        driver.get("https://demowebshop.tricentis.com/");
+        FooterTestFlow footerTestFlow = new FooterTestFlow(driver);
+        footerTestFlow.verifyFooterComponent();
+    }
+}
