1c021fad31266d44e5cfc904821c3144623cfec2
Khanh Duong <khanhduong4036@gmail.com>
2023-12-20 15:15:29 +0700

    Fix comment topcartlink

--- src/main/java/models/components/global/footer/HeaderComponent.java
+++ src/main/java/models/components/global/footer/HeaderComponent.java
@@ -5,19 +5,20 @@ import models.components.ComponentCSSSelector;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import support.ui.ExpectedConditionsEx;
 
 @ComponentCSSSelector(value = ".header")
 public class HeaderComponent extends Component {
 
-    private static final By shoppingCartLinkSel = By.id("topcartlink");
+    private static final By shoppingCartLinkSel = By.cssSelector("li#topcartlink");
 
     public HeaderComponent(WebDriver driver, WebElement component) {
         super(driver, component);
     }
 
     public void clickOnShoppingCartLink(){
+        wait.until(ExpectedConditions.elementToBeClickable(shoppingCartLinkSel));
         findElement(shoppingCartLinkSel).click();
     }
-
-
 }
